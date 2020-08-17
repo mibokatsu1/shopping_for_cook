33400bbb42913ff93890235bdd4fca54354409d9
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-17 18:54:21 +0900

    add routes menu#new

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,7 @@
 class MenusController < ApplicationController
   def index
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'menus/index'
   root "menus#index"
+  resources :menus
 end
