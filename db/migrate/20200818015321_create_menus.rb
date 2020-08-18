de204cd5d2da53d612057f25cfdb698b88970513
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-18 16:30:21 +0900

    create model about menu

---
+++ db/migrate/20200818015321_create_menus.rb
@@ -0,0 +1,10 @@
+class CreateMenus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :menus do |t|
+      t.string :name, null: false
+      t.index :name, unique: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
