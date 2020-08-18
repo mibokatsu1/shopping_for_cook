2997075c832eaefa12997a04daa4989536149839
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-18 16:34:51 +0900

    create Unit-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_064451) do
+ActiveRecord::Schema.define(version: 2020_08_18_073131) do
 
   create_table "ingredients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 2020_08_18_064451) do
     t.index ["user_id"], name: "index_menus_on_user_id"
   end
 
+  create_table "units", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "unit", null: false
+    t.bigint "ingredient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_units_on_ingredient_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -65,4 +73,5 @@ ActiveRecord::Schema.define(version: 2020_08_18_064451) do
   add_foreign_key "menu_items", "items"
   add_foreign_key "menu_items", "menus"
   add_foreign_key "menus", "users"
+  add_foreign_key "units", "ingredients"
 end
