0009602448053342e5bdc3ce8987d9fb683e5428
mibokatsu1 <mibo.katsu@gmail.com>
2020-08-18 18:07:36 +0900

    add params into items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item = Item.find(params[:id])
+  end
+
+private
+  def item_params
+    marge(@item)
+    params.require(:item).permit(:amount, :menu_id, :ingredient_id)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
